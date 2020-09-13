614ec2f4be29b4baf3d8fe271de37b5f5a9ef427
kaushalshabi <kaushalshabi@gmail.com>
2020-09-14 02:09:55 +0530

    added new file q104.py

---
+++ q104.py
@@ -0,0 +1,15 @@
+n = [20, 5, 15, 10, 0]
+n.sort()
+print(n)
+
+s=["Dog","Banana","Cat","Apple"]
+s.sort()
+print(s)   #['Apple','Banana','Cat','Dog']
+
+n = [40, 10, 30, 20]
+n.sort()
+print(n)
+n.sort(reverse=True)
+print(n)
+n.sort(reverse=False)
+print(n) == > [10, 20, 30, 40
