5f6358970a3aeb21afed168fb8818678dfd23bd2
kaushalshabi <kaushalshabi@gmail.com>
2020-09-17 19:57:16 +0530

    q130

---
+++ q130.py
@@ -0,0 +1,17 @@
+n=int(input("Enter the number of students: "))
+d={}
+for i in range(n):
+    name=input("Enter Student Name: ")
+    marks=input("Enter Student Marks: ")
+    d[name]=marks
+    while True:
+        name=input("Enter Student Name to get Marks: ")
+        marks=d.get(name,-1)
+        if marks== -1:
+            print("Student Not Found")
+        else:
+            print("The Marks of",name,"are",marks)
+            option=input("Do you want to find another student marks[Yes|No]")
+        if option=="No":
+                break
+ print("Thanks for using our application")
