d8b18a718397b584e83637a6ec5170ceba80f67c
kaushalshabi <kaushalshabi@gmail.com>
2020-09-17 20:22:10 +0530

    q194

---
+++ q194.py
@@ -0,0 +1,12 @@
+from random import *
+for i in range(10):
+    print(randrange(10))
+
+
+from random import *
+for i in range(10):
+    print(randrange(1,11))
+
+from random import *
+for i in range(10):
+    print(randrange(1,11,2))
