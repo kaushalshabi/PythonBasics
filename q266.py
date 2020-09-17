122a93610ca88230e9a14f28234083146b194ab0
kaushalshabi <kaushalshabi@gmail.com>
2020-09-17 20:44:33 +0530

    q266

---
+++ q266.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number:"))
+for i in range(1,num+1):
+    print("  "*(i-1),end="")
+    for j in range(1,num+2-i):
+        print("*",end=" ")
+    for k in range(1,num+1-i):
+        print("*",end=" ")
+    print()
