36e286ec2a5422bf91f75ee6e5f8ed428e0fd116
kaushalshabi <kaushalshabi@gmail.com>
2020-09-17 20:24:26 +0530

    q202

---
+++ q202.py
@@ -0,0 +1,13 @@
+try:
+    print("outer try block")
+    try:
+        print("Inner try block")
+        print(10/0)
+    except ZeroDivisionError:
+        print("Inner except block")
+    finally:
+        print("Inner finally block")
+except:
+    print("outer except block")
+finally:
+    print("outer finally block") 
