86f3a08d0a535795a38294b0261ccfa4fb4a9002
kaushalshabi <kaushalshabi@gmail.com>
2020-09-17 20:25:20 +0530

    q205

---
+++ q205.py
@@ -0,0 +1,9 @@
+class Test:
+    def __init__(self):
+        print("Constructor exeuction...")
+    def m1(self):
+        print("Method execution...")
+t1 = Test()
+t2 = Test()
+t3 = Test()
+t1.m1()
