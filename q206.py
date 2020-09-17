9cd0cc90ce9f4c4f595258662a495f49ec739863
kaushalshabi <kaushalshabi@gmail.com>
2020-09-17 20:25:38 +0530

    q206

---
+++ q206.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,x,y,z):
+        self.name=x
+        self.rollno=y
+        self.marks=z
+    def display(self):
+        print("Student Name:{}\nRollno:{} \nMarks:{}".format(self.name,self.rollno,self.marks) )
+s1=Student("Durga",101,80)
+s1.display()
+s2=Student("Sunny",102,100)
+s2.display()
