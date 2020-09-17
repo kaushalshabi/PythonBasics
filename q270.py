38eccd3a00029fb0adf192c2da61890b24a8a36c
kaushalshabi <kaushalshabi@gmail.com>
2020-09-17 20:45:40 +0530

    q270

---
+++ q270.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number:"))
+for i in range(1,num+1):
+    print("  "*(i-1),end="")
+    for j in range(1,num+2-i):
+        print(chr(65+num-i),end=" ")
+    for k in range(2,num+2-i):
+        print(chr(65+num-i),end=" ")
+    print()
