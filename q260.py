6bf8189a996b4c055bfb58186383591b431714b2
kaushalshabi <kaushalshabi@gmail.com>
2020-09-17 20:43:00 +0530

    q260

---
+++ q260.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    print("  " * (n - i), end="")
+    for j in range(65 + 2 * i - 2, 64, -1):
+        print(chr(j), end=" ")
+    print()
